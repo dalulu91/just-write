c3b7e5e0bfd6df8c3b9c8e46c2aae58d50691f6e
dalulu91 <danielluras@live.no>
2024-10-11 21:54:46 +0200

    - Refactor, bug fixes, dynamic text formatting, and styling. 1. Fixed a problem with the controller and the mainwindow dealing with seperate instances of headframe and textframe. 2. Double click for fullscreen is now limited to the header only 3. Dragging the window is now limited to the header only
    Still need to fix issues with keybindings and the tag-stack

--- main.py
+++ main.py
@@ -4,73 +4,124 @@ from datetime import datetime
 
 
 def main():
-    style = Style()
-
-    app = MainWindow(style)
-    head = HeadFrame(app, style)
-    text = TextFrame(app, style)
-    controller = Controller(app, head, text)
+    app = MainWindow()
+    controller = Controller(app)
 
     controller.run()
 
 
 class MainWindow(tk.Tk):
-    def __init__(self, style):
+    def __init__(self):
         super().__init__()
 
-        # Colors
-        self.DARK = "#121319"
-        self.LIGHT = "#d5ae88"
-        self.FONT = "AGaramond"
+        style = Style()
 
         # Window settings
         self.title("My Writing App")
-        self.geometry("700x800+1100+100")
+        self.geometry(f"{style.windowWidth}x{style.windowHeight}+608+62")
         self.overrideredirect(True)
         self.saved_geometry = self.geometry()
         self.is_fullscreen = False
-        self.config(bg=self.DARK)
+        self.config(bg=style.background)
 
         # Layout
         self.headFrame = HeadFrame(self)
         self.textFrame = TextFrame(self)
 
         # Header
-        self.headFrame.config(bg=self.DARK)
+        self.headFrame.config(bg=style.background)
         self.headFrame.pack(fill="x", pady=10, padx=20, anchor="n")
 
         # Textframe
-        self.textFrame.config(width=600, bg=self.DARK)
+        self.textFrame.config(width=style.textWidth, bg=style.background)
         self.textFrame.pack(fill="y", padx=100, expand=True, anchor="n")
 
 
+class HeadFrame(tk.Frame):
+    def __init__(self, mainwindow):
+        super().__init__(mainwindow)
+
+        self.mainwindow = mainwindow
+        style = Style()
+
+        title = tk.Label(
+            self, text="just write", fg=style.fontColor2, bg=style.background
+        )
+        title.pack(pady=20)
+        title.configure(font=(style.font, 12, "italic"))
+
+
+class TextFrame(tk.Frame):
+    def __init__(self, mainwindow):
+        super().__init__(mainwindow)
+
+        self.mainwindow = mainwindow
+        style = Style()
+
+        with open("placeholdertext.txt", "r", encoding="utf-8") as file:
+            placeholdertext = file.read()
+
+        self.textarea = tk.Text(self)
+        self.textarea.pack(fill="both", expand=True, pady=30)
+        self.textarea.config(
+            bg=style.background,
+            fg=style.fontColor1,
+            borderwidth=0,
+            insertbackground=style.fontColor1,
+            wrap="word",
+            tabs=(30),
+            spacing1=(8),
+            spacing2=(5),
+            spacing3=(5),
+            undo=True,
+            font=(style.font, style.fontSize),
+        )
+
+        self.textarea.insert("1.0", placeholdertext)
+        self.textarea.tag_configure("bold", font=(style.font, style.fontSize, "bold"))
+        self.textarea.tag_configure(
+            "italic", font=(style.font, style.fontSize, "italic")
+        )
+        self.textarea.tag_configure("underline", underline=True)
+        self.textarea.tag_configure("h1", font=(style.font, style.h1))
+        self.textarea.tag_configure("h2", font=(style.font, style.h2))
+        self.textarea.tag_configure("h3", font=(style.font, style.h3))
+        self.textarea.tag_configure("center", justify="center")
+        self.textarea.tag_configure("left", justify="left")
+        self.textarea.tag_configure("right", justify="right")
+        self.textarea.tag_configure("color1", foreground=style.fontColor1)
+        self.textarea.tag_configure("color2", foreground=style.fontColor2)
+        self.textarea.tag_configure("color3", foreground=style.fontColor3)
+
+
 class Controller:
-    def __init__(self, MainWindow, HeadFrame, TextFrame):
+    def __init__(self, MainWindow):
         self.mainwindow = MainWindow
-        self.headframe = HeadFrame
-        self.textframe = TextFrame
+        self.headframe = self.mainwindow.headFrame
+        self.textframe = self.mainwindow.textFrame
 
         #  WARN: Exit on <Escape> is developer mode only.
         #        Must remove before deployment.
         self.mainwindow.bind("<Escape>", lambda _e: self.mainwindow.destroy())
 
-        self.mainwindow.bind("<ButtonPress-3>", self.start_drag)
-        self.mainwindow.bind("<B3-Motion>", self.drag_window)
-        self.mainwindow.bind("<Double-Button-1>", self.toggleFullscreen)
+        self.headframe.bind("<ButtonPress-1>", self.start_drag)
+        self.headframe.bind("<B1-Motion>", self.drag_window)
+        self.headframe.bind("<Double-Button-1>", self.toggleFullscreen)
 
-    def apply_tag(self, tag_name):
-        try:
-            # Sjekker om det er markert tekst
-            if not self.textframe.textarea.tag_ranges(tk.SEL):
-                print("Ingen tekst markert for tagging.")
-                return  # Hvis ingen tekst er markert, stopper vi her.
-
-            start, end = self.textframe.textarea.tag_ranges(tk.SEL)
-            print(f"Tagging fra {start} til {
-                  end} med {tag_name}")  # Debug output
-            self.textframe.textarea.tag_add(tag_name, start, end)
-        except Exception as e:
-            print(e)
+        #  TODO: Find out how to reset/unbind default binding.
+        #        Ctrl+[1, 2, 3...] doesn't work.
+        self.textframe.textarea.bind("<Control-b>", self.make_bold)
+        self.textframe.textarea.bind("<Control-e>", self.make_italic)
+        self.textframe.textarea.bind("<Control-u>", self.make_underline)
+        self.textframe.textarea.bind("<F1>", self.make_h1)
+        self.textframe.textarea.bind("<F2>", self.make_h2)
+        self.textframe.textarea.bind("<F3>", self.make_h3)
+        self.textframe.textarea.bind("<F4>", self.make_center)
+        self.textframe.textarea.bind("<F5>", self.make_left)
+        self.textframe.textarea.bind("<F6>", self.make_right)
+        self.textframe.textarea.bind("<Button-2>", self.make_color1)
+        self.textframe.textarea.bind("<Button-4>", self.make_color2)
+        self.textframe.textarea.bind("<Button-5>", self.make_color3)
 
     def make_bold(self, _event=None):
         self.apply_tag("bold")
@@ -81,11 +132,47 @@ class Controller:
     def make_underline(self, _event=None):
         self.apply_tag("underline")
 
-    def make_title(self, _event=None):
-        self.apply_tag("title")
+    def make_h1(self, _event=None):
+        self.apply_tag("h1")
 
-    def make_subtitle(self, _event=None):
-        self.apply_tag("subtitle")
+    def make_h2(self, _event=None):
+        self.apply_tag("h2")
+
+    def make_h3(self, _event=None):
+        self.apply_tag("h3")
+
+    def make_center(self, _event=None):
+        self.apply_tag("center")
+
+    def make_left(self, _event=None):
+        self.apply_tag("left")
+
+    def make_right(self, _event=None):
+        self.apply_tag("right")
+
+    def make_color1(self, _event=None):
+        self.apply_tag("color1")
+
+    def make_color2(self, _event=None):
+        self.apply_tag("color2")
+
+    def make_color3(self, _event=None):
+        self.apply_tag("color2")
+
+    #  TODO: Find out how to reset/delete previous tag when applying a new one
+    def apply_tag(self, tag_name):
+        try:
+            start, end = self.textframe.textarea.tag_ranges(tk.SEL)
+            self.textframe.textarea.tag_add(tag_name, start, end)
+        except Exception as e:
+            print(e)
+
+    def delete_tag(self, tag_name):
+        try:
+            start, end = self.textframe.textarea.tag_ranges(tk.SEL)
+            self.textframe.textarea.tag_remove(tag_name, start, end)
+        except Exception as e:
+            print(e)
 
     def toggleFullscreen(self, _event=None):
         if self.mainwindow.is_fullscreen:
@@ -116,65 +203,6 @@ class Controller:
             self.mainwindow.y = event.y
 
 
-class HeadFrame(tk.Frame):
-    def __init__(self, mainwindow, style=None):
-        super().__init__(mainwindow)
-
-        self.mainwindow = mainwindow
-        self.FONT_SIZE = 12
-        self.Title()
-
-    def Title(self):
-        title = tk.Label(
-            self, text="just write", fg=self.mainwindow.LIGHT, bg=self.mainwindow.DARK
-        )
-        title.pack(pady=20)
-        title.configure(font=(self.mainwindow.FONT, self.FONT_SIZE, "italic"))
-
-
-class TextFrame(tk.Frame):
-    def __init__(self, mainwindow, style=None):
-        super().__init__(mainwindow)
-
-        self.mainwindow = mainwindow
-
-        self.FONT_COLOR = "#d5d5d5"
-        self.FONT_SIZE_NORMAL = 12
-        self.FONT_SIZE_TITLE = 24
-        self.FONT_SIZE_SUBTITLE = 18
-
-        self.textarea = tk.Text(self)
-        self.textarea.pack(fill="both", expand=True, pady=30)
-        self.textarea.config(
-            bg=self.mainwindow.DARK,
-            fg=self.FONT_COLOR,
-            borderwidth=0,
-            insertbackground=self.FONT_COLOR,
-            wrap="word",
-            tabs=(30),
-            undo=True,
-            font=(self.mainwindow.FONT, self.FONT_SIZE_NORMAL),
-        )
-
-        self.textarea.tag_configure(
-            "bold", font=(self.mainwindow.FONT, self.FONT_SIZE_NORMAL, "bold")
-        )
-        self.textarea.tag_configure(
-            "italic", font=(self.mainwindow.FONT, self.FONT_SIZE_NORMAL, "italic")
-        )
-        self.textarea.tag_configure(
-            "underline",
-            font=(self.mainwindow.FONT, self.FONT_SIZE_NORMAL),
-            underline=True,
-        )
-        self.textarea.tag_configure(
-            "title", font=(self.mainwindow.FONT, self.FONT_SIZE_TITLE, "bold")
-        )
-        self.textarea.tag_configure(
-            "subtitle", font=(self.mainwindow.FONT, self.FONT_SIZE_SUBTITLE, "italic")
-        )
-
-
 class Document:
     def __init__(self):
         self.filename: str
@@ -185,10 +213,21 @@ class Document:
 # Colors, fonts, styling
 class Style:
     def __init__(self):
-        # Colors
-        self.DARK = "#121319"
-        self.LIGHT = "#d5ae88"
-        self.FONT = "AGaramond"
+        self.windowWidth = "800"
+        self.windowHeight = "950"
+        self.textWidth = 600
+        self.background = "#121319"
+        self.fontColor1 = "#d5d5d5"
+        self.fontColor2 = "#d5ae88"
+        self.fontColor3 = "#0eb6d4"
+        self.font = "AGaramond"
+        self.fontSize = 14
+        self.h1 = 36
+        self.h2 = 24
+        self.h3 = 18
+
+        #  TODO: Set 3 font sizes: size1, size2, and size3.
+        #        The user can multiply these sizes relatively.
 
 
 if __name__ == "__main__":
